71f9d99e18d546ee28f3103d65fd4813b24b88e1
Mattias Andrée <maandree@operamail.com>
2015-05-18 21:40:31 +0200

    sit-category: parse cmdline
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/sit-category.c
+++ src/sit-category.c
@@ -29,16 +29,83 @@
 
 
 
+#define CMD(a, b)  CMD_TEST(command, a, (b) || !ticket_id)
+
+
+
 char *argv0;
+
+static int opt_critical = 0, opt_print_id = 0;
+static char *ticket_id = NULL;
+static size_t category_count = 0;
+static char **categories = NULL;
+
+
+
+static int
+add_categories(void)
+{
+	return 1;
+}
+
+
+static int
+remove_categories(void)
+{
+	return 1;
+}
+
+
+static int
+list_categories(void)
+{
+	return 1;
+}
 
 
 int
 main(int argc, char *argv[])
 {
-	argv0 = *argv;
+	char *command;
+	int r = 0;
 
 	HELP("Add, remove or list categories of an issue");
 
-	return EXIT_SUCCESS;
+	/* Parse command line. */
+	ARG_BEGIN (argv0, command) {
+		ARG_USAGE;
+		ARG_BOOLEAN('c', opt_critical);
+		ARG_BOOLEAN('p', opt_print_id);
+	ARG_NONOPT:
+		if (!ticket_id) {
+			ticket_id = *argv;
+		} else {
+			if (!categories) {
+				categories = malloc(argc * sizeof(char*));
+				if (!categories) {
+					goto fail;
+				}
+			}
+			categories[category_count++] = *argv;
+		}
+		break;
+	} ARG_END;
+
+	/* Select action. */
+	if (CMD("add", opt_print_id)) {
+		r = add_categories();
+	} else if (CMD("remove", opt_print_id)) {
+		r = remove_categories();
+	} else if (CMD("list", opt_critical || category_count)) {
+		r = list_categories();
+	} else {
+		USAGE();
+	}
+	FAIL_TEST(r);
+
+	/* Return and cleanup. */
+	DONE_MAIN;
+	free(categories);
+	DONE;
 }
 
